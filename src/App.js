5a9ea118923867ec5214daec44cb466e2a0384de
Akshay R <cbptrm@gmail.com>
2021-09-22 23:04:08 +0530

    second

--- src/App.js
+++ src/App.js
@@ -10,18 +10,25 @@ function App() {
   const [followers, setfollowes] = useState('');
   const [userInput, setuserInput] = useState('');
   const [error,setError]= useState('');
+  const[init,setInit]= useState('true');
 
   useEffect(() => {
+    if(init===true){
+      setInit(false);
+    }
+    
     fetch(" https://api.github.com/users")
     .then(res => res.json())
     .then(data => {
       
         setData(data);
+        console.log("hello");
       
       
-    });
+    } );
    
-  }, []);
+  }, [init]);
+ 
 
   const setData =({name,login,followers}) =>{
     setName(name);
@@ -62,21 +69,23 @@ function App() {
 
      </div>
      <div className="white">
+     <div className="result">
       <div className="search">
-       <input type="text" placeholder="Search.." onChange={handleSearch}></input><button type="submit" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+       <input type="text" placeholder="Search.." onChange={handleSearch}></input><button type="submit" onClick={handleSubmit}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
   <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
 </svg></button>
-<button type="submit"  onClick={handleSubmit}>submit</button>
+
 </div>
 { error ? (<h1>{error}</h1>) : (
-  <div className="result">
+  <div>
   <div> {name}</div>
   <div>{userName}</div>
   <div>{followers} followers</div>
-  </div>
   
+  </div>
       
 ) }
+</div>
  </div>
       </div>
 
